332e73d2693a11386693644dffcc09354ab7700a
Vishal Kaslwial <vishal.kasliwal@gmail.com>
2017-06-29 18:04:49 -0700

    Revised a little bit

---
+++ SConstruct
@@ -0,0 +1,24 @@
+import os
+
+env = Environment(ENV = os.environ, CC='icc', CXX='icpc')
+env.Append(CPPFLAGS=['-xhost','-Wall','-qopenmp','-qopenmp-simd','-qopt-report'])
+env.Append(LINKFLAGS=['-qopenmp','-qopenmp-simd'])
+env.Append(CPPPATH='/opt/intel/advisor/include')
+#conf = Configure(env)
+#if not conf.CheckCXXHeader('advisor-annotate.h'):
+#    print 'advisor-annotate.h must be installed!'
+#    Exit(1)
+#if not conf.CheckLibWithHeader('m', 'math.h', 'c'):
+#    print 'Did not find libm'
+#if not conf.CheckLibWithHeader('memkind', 'hbwmalloc.h', 'c'):
+#    print 'Did not find libmemkind'
+#else:
+#    env.Append(CCFLAGS='-D__HBM__')
+#env = conf.Finish()
+debug = ARGUMENTS.get('debug', 0)
+if int(debug):
+    env.Append(CCFLAGS=['-g','-O0'])
+else:
+    env.Append(CCFLAGS=['-g','-O2'])
+
+env.Program('luDecomp', ['main.cc'])
--- main.cc
+++ main.cc
@@ -6,7 +6,301 @@
 
 #include "advisor-annotate.h"
 
-#include "lu.cc"
+#define PROBLEM_SIZE 256
+#define NUM_MATRICES 100
+#define NUM_TRIALS 10
+
+//#define IJK
+//#define IJK_PAR
+//#define IJK_VEC
+//#define IJK_OPT
+//#define IJK_SUPER
+//#define IKJ
+//#define IKJ_VEC
+//#define KIJ
+//#define KIJ_VEC
+//#define KIJ_PAR
+#define KIJ_OPT
+
+void LU_decomp_ijk(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = i; j < n; ++j) {
+      for (int k = 0; k < i; ++k) {
+        A[i*lda + j] -= A[i*lda + k]*A[k*lda + j];
+      }
+    }
+    for (int j = i + 1; j < n; ++j) {
+      for (int k = 0; k < i; ++k) {
+        A[j*lda + i] -= A[j*lda + k]*A[k*lda + i];
+      }
+      A[j*lda + i] /= A[i*lda + i];
+    }
+  }
+}
+
+void LU_decomp_ijk_par(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  for (int i = 0; i < n; ++i) {
+#pragma omp parallel for
+    for (int j = i; j < n; ++j) {
+      for (int k = 0; k < i; ++k) {
+        A[i*lda + j] -= A[i*lda + k]*A[k*lda + j];
+      }
+    }
+#pragma omp parallel for
+    for (int j = i + 1; j < n; ++j) {
+      for (int k = 0; k < i; ++k) {
+        A[j*lda + i] -= A[j*lda + k]*A[k*lda + i];
+      }
+      A[j*lda + i] /= A[i*lda + i];
+    }
+  }
+}
+
+void LU_decomp_ijk_vec(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  const int cache_line = 8, num_threads = omp_get_max_threads();
+  double * holders = (double*)_mm_malloc(sizeof(double)*num_threads*cache_line, 64);
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = i; j < n; ++j) {
+      int tid = omp_get_thread_num();
+      holders[cache_line*tid] = A[i*lda + j];
+#pragma simd
+#pragma ivdep
+      for (int k = 0; k < i; ++k) {
+        holders[cache_line*tid] -= A[i*lda + k]*A[k*lda + j];
+        }
+        A[i*lda + j] = holders[cache_line*tid];
+    }
+    for (int j = i + 1; j < n; ++j) {
+      int tid = omp_get_thread_num();
+      holders[cache_line*tid] = A[j*lda + i];
+#pragma simd
+#pragma ivdep
+        for (int k = 0; k < i; ++k) {
+          holders[cache_line*tid] -= A[j*lda + k]*A[k*lda + i];
+        }
+        A[j*lda + i] = holders[cache_line*tid]/A[i*lda + i];
+    }
+  }
+  _mm_free(holders);
+}
+
+void LU_decomp_ijk_opt(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  const int cache_line = 8, num_threads = omp_get_max_threads();
+  double * holders = (double*)_mm_malloc(sizeof(double)*num_threads*cache_line, 64);
+
+#pragma omp parallel
+{
+  for (int i = 0; i < n; ++i) {
+#pragma omp for schedule(static)
+    for (int j = i; j < n; ++j) {
+      int tid = omp_get_thread_num();
+      holders[cache_line*tid] = A[i*lda + j];
+#pragma simd
+#pragma ivdep
+      for (int k = 0; k < i; ++k) {
+        holders[cache_line*tid] -= A[i*lda + k]*A[k*lda + j];
+        }
+        A[i*lda + j] = holders[cache_line*tid];
+    }
+#pragma omp for schedule(static)
+    for (int j = i + 1; j < n; ++j) {
+      int tid = omp_get_thread_num();
+      holders[cache_line*tid] = A[j*lda + i];
+#pragma simd
+#pragma ivdep
+        for (int k = 0; k < i; ++k) {
+          holders[cache_line*tid] -= A[j*lda + k]*A[k*lda + i];
+        }
+        A[j*lda + i] = holders[cache_line*tid]/A[i*lda + i];
+    }
+  }
+}
+  _mm_free(holders);
+}
+
+void LU_decomp_ijk_super(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+  const int cache_line = 8, num_threads =  sysconf(_SC_NPROCESSORS_ONLN);
+  omp_set_num_threads(num_threads);
+  double * ATran = (double*)_mm_malloc(sizeof(double)*n*lda + 64, 64);
+#pragma omp parallel for
+  for (int rowCtr = 0; rowCtr < n; ++rowCtr) {
+#pragma simd
+#pragma ivdep
+    for (int colCtr = 0; colCtr < n; ++colCtr) {
+      ATran[colCtr*lda + rowCtr] = A[rowCtr*lda + colCtr];
+    }
+  }
+  double * holders = (double*)_mm_malloc(sizeof(double)*num_threads*cache_line, 64);
+
+#pragma omp parallel
+{
+  for (int i = 0; i < n; ++i) {
+#pragma omp for schedule(static)
+    for (int j = i; j < n; ++j) {
+      int tid = omp_get_thread_num();
+      holders[cache_line*tid] = A[i*lda + j];
+#pragma simd
+#pragma ivdep
+      for (int k = 0; k < i; ++k) {
+        holders[cache_line*tid] -= A[i*lda + k]*ATran[j*lda + k];
+      }
+      A[i*lda + j] = holders[cache_line*tid];
+      ATran[j*lda + i] = A[i*lda + j];
+    }
+#pragma omp for schedule(static)
+    for (int j = i + 1; j < n; ++j) {
+      int tid = omp_get_thread_num();
+      holders[cache_line*tid] = A[j*lda + i];
+#pragma simd
+#pragma ivdep
+      for (int k = 0; k < i; ++k) {
+        holders[cache_line*tid] -= A[j*lda + k]*ATran[i*lda + k];
+      }
+      A[j*lda + i] = holders[cache_line*tid]/A[i*lda + i];
+      ATran[i*lda + j] = A[j*lda + i];
+    }
+  }
+}
+    _mm_free(holders);
+    _mm_free(ATran);
+}
+
+void LU_decomp_ikj(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  for (int i = 1; i < n; i++) {
+    for (int k = 0; k < i; k++) {
+      A[i*lda + k] = A[i*lda + k]/A[k*lda + k];
+#pragma novector
+      for (int j = k+1; j < n; j++) 
+        A[i*lda + j] -= A[i*lda+k]*A[k*lda + j];
+    }
+  }
+}
+
+void LU_decomp_ikj_vec(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  for (int i = 1; i < n; i++) {
+    for (int k = 0; k < i; k++) {
+      A[i*lda + k] = A[i*lda + k]/A[k*lda + k];
+#pragma simd
+#pragma ivdep
+	  for (int j = k+1; j < n; j++) 
+        A[i*lda + j] -= A[i*lda+k]*A[k*lda + j];
+    }
+  }
+}
+
+void LU_decomp_kij(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  for (int k = 0; k < n-1; k++) {
+    const double recAkk = 1.0/A[k*lda + k];
+    for (int i = k+1; i < n; i++) {
+      A[i*lda + k] = A[i*lda + k]*recAkk;
+#pragma novector
+      for (int j = k+1; j < n; j++)
+        A[i*lda + j] -= A[i*lda + k]*A[k*lda + j];
+    }
+  }
+}
+
+void LU_decomp_kij_vec(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+  for (int k = 0; k < n-1; k++) {
+    const double recAkk = 1.0/A[k*lda + k];
+    for (int i = k+1; i < n; i++) {
+      A[i*lda + k] = A[i*lda + k]*recAkk;
+      #pragma simd
+      #pragma ivdep
+      for (int j = k+1; j < n; j++)
+	    A[i*lda + j] -= A[i*lda+k]*A[k*lda + j];
+    }
+  }
+}
+
+void LU_decomp_kij_par(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+#pragma omp parallel
+{
+  for (int k = 0; k < n-1; k++) {
+    const double recAkk = 1.0/A[k*lda + k];
+#pragma omp for
+    for (int i = k+1; i < n; i++) {
+      A[i*lda + k] = A[i*lda + k]*recAkk;
+#pragma novector
+      for (int j = k+1; j < n; j++)
+        A[i*lda + j] -= A[i*lda+k]*A[k*lda + j];
+    }
+  }
+}
+}
+
+void LU_decomp_kij_opt(const int n, const int lda, double* const A) {
+  // LU decomposition without pivoting (Doolittle algorithm)
+  // In-place decomposition of form A=LU
+  // L is returned below main diagonal of A
+  // U is returned at and above main diagonal
+
+#pragma omp parallel
+{
+  for (int k = 0; k < n-1; k++) {
+    const double recAkk = 1.0/A[k*lda + k];
+#pragma omp for
+    for (int i = k+1; i < n; i++) {
+      A[i*lda + k] = A[i*lda + k]*recAkk;
+      #pragma simd
+      #pragma ivdep
+      for (int j = k+1; j < n; j++)
+	    A[i*lda + j] -= A[i*lda+k]*A[k*lda + j];
+    }
+  }
+}
+}
+
 
 /*****************************************************************************/
 
@@ -93,9 +387,9 @@ _mm_free(U);
 int main(const int argc, const char** argv) {
 
   // Problem size and other parameters
-  const int n=256;
+  const int n=PROBLEM_SIZE;
   const int lda=n+16;
-  const int nMatrices=100;
+  const int nMatrices=NUM_MATRICES;
   const double HztoPerf = 1e-9*2.0/3.0*double(n*n*static_cast<double>(lda))*nMatrices;
 
   const size_t containerSize = sizeof(double)*n*lda+64;
@@ -153,7 +447,7 @@ int main(const int argc, const char** argv) {
 #endif
 
   double rate = 0, dRate = 0; // Benchmarking data
-  const int nTrials = 10;
+  const int nTrials = NUM_TRIALS;
   const int skipTrials = 3; // First step is warm-up on Xeon Phi coprocessor
   printf("\033[1m%5s %10s %8s\033[0m\n", "Trial", "Time, s", "GFLOP/s");
   for (int trial = 1; trial <= nTrials; trial++) {
